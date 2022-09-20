44de655d70593b0713a053ec485d04e057dc70ef
Git-punam <dutta.punam.2017@gmail.com>
2022-09-20 20:55:11 +0530

    feat: late fine/charges calculation done and optimized

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,30 +1,7 @@
 import {adminModel, bookModel, studentModel} from '../models/dbModels.js'
 import { generateTokenAdmin } from '../utils/generateToken.js'
+import { returnDateCalc, fineCalc } from '../utils/calc.js'
 import bcrypt from 'bcrypt'
-
-
-
-
-//object for months having 31 days
-const arr31 = {
-    1 : "january",
-    3 : "march",
-    5:  "may",
-    7:  "july",
-    8: "august",
-    10: "october",
-    12 : "december"
-}
-
-// object for months having 30 days
-const arr30 = {
-    4 : "april",
-    6 : "june",
-    9:  "september",
-    11:  "november",
-}
-
-
 
 
 /**
@@ -108,7 +85,7 @@ export const adminLogin = async(req,res)=>{
  * @access private
  */
 export const updateAdminProfile = async(req,res)=>{
-    const { adminId} = req.params
+    const { adminId } = req.params
     try{
         let { name, username, password, email } = req.body
         if(password) password = await bcrypt.hash(password,10);
@@ -136,7 +113,6 @@ export const updateAdminProfile = async(req,res)=>{
         })
     }
 }
-
 
 
 
@@ -219,48 +195,21 @@ export const issueBook = async(req,res)=>{
         year:1,
         roll :1
         })
-
-    // Return_date calculation
-    //Issue_date format:dd-mm-yyyy
-        let dd = 7 + Number(Issue_date.substr(0,2));//'substr(0,2)' extract substring from position 0 with length=2
-        let mm = Number(Issue_date.substr(3,2));//'substr(3,2)' extract substring from position 3 with length=2
-        let yyyy = Number(Issue_date.substr(6));//'substr(6)' extract rest of the substring from position 6
-
-        //if the month is apr/jun/sep/nov
-        if(Object.keys(arr30).includes(mm.toString())){
-            if(Math.floor(dd/30) > 0) mm = mm + 1;
-            dd = dd % 30;
-        }   
-        //if the month is jan/mar/may/jul/aug/oct/dec  
-        else if(Object.keys(arr31).includes(mm.toString())) {
-            if(Math.floor(dd/31) > 0) mm = mm + 1;
-            dd = dd % 31;
-        }       
-        else {
-            //checking leap year or not
-                if( yyyy % 4 == 0 ) {
-                    if( Math.floor(dd/29)>0) mm = mm + 1;
-                    dd = dd % 29;
-                }
-                else { 
-                    if( Math.floor(dd/28)>0) mm = mm + 1;
-                    dd = dd % 28;
-                }
-            }
-        if( Math.floor(mm/12) > 0) {yyyy = yyyy + 1; mm = mm % 12;}
-        let ddS = (dd < 10) ? ('0'+ dd.toString()) : dd.toString()
-        let mmS = (mm < 10) ? ('0'+ mm.toString()) : mm.toString()
-        let yyS = yyyy.toString()
-        let Return_date = ddS + '-'+ mmS + '-' + yyS 
-        
-        
+        let Return_date = '';
+        if(studentDetails) Return_date = returnDateCalc(Issue_date,Return_date);
+        else{
+            res.status(400).json({
+                status: false,
+                messgae : "The student does not exist"
+            })
+        }
         const issueBook = await bookModel.findByIdAndUpdate({_id:bookId},{
             Issue_date, Return_date, Student_ID})
         if(issueBook){
             res.status(200).json({
                 success : true,
                 message : "The selected book is issued successfully",
-                data : studentDetails
+                data : Return_date
             })
         }
         else{
@@ -281,67 +230,28 @@ catch(error){
 
 
 /**
- * @description Return book 
+ * @description Return book (when a book is returned, late fine/charges will be calculated automatically based on the actual return date)
  * @route PUT /api/admin/return-book/:bookId
  * @access private
  */
  export const returnBook = async(req,res)=>{
-    const {bookId} = req.params
+    const { bookId } = req.params
     const { Actual_return_date } = req.body
     try{
             const findTheBook = await bookModel.findOne({_id:bookId},{Return_date:1})
-            let Charges_Rs=0;
-            if(findTheBook)
-            {
-                //Charges_rs calculation
-                //Issue_date,Actual_date format:dd-mm-yyyy
-                let yyyyReturn = Number(findTheBook.Return_date.substring(6))//'substring(6)' extract rest of the substring from position 6
-                let yyyyActual = Number(Actual_return_date.substring(6))
-
-                let mmReturn = Number(findTheBook.Return_date.substring(3,5))//'substr(3,5)' extract substring from position 3 to 4
-                let mmActual = Number(Actual_return_date.substring(3,5))
-
-                let ddReturn = Number(findTheBook.Return_date.substring(0,2))//'substring(0,2)' extract substring from position 0 to 1
-                let ddActual = Number(Actual_return_date.substring(0,2))
-
-                //book returned in the same year
-                if(yyyyActual-yyyyReturn == 0)
-                {
-                    //book returned in the same month
-                    if(mmActual-mmReturn == 0) Charges_Rs = ddActual - ddReturn; 
-                    //book returned in later months
-                    else if(mmActual-mmReturn > 0)
-                    {
-                        //if the month is apr/jun/sep/nov in Return_date
-                        if(Object.keys(arr30).includes(mmReturn.toString())) Charges_Rs = 30 - ddReturn;
-                            //if the month is jan/mar/may/jul/aug/oct/dec in Return_date 
-                        else if(Object.keys(arr31).includes(mmReturn.toString())) Charges_Rs = 31 - ddReturn;
-                        else 
-                        {
-                            if( yyyyReturn % 4 == 0 ) Charges_Rs = 29 - ddReturn;
-                            else Charges_Rs = 28 - ddReturn;
-                        }
-                        let count30 = 0;
-                        let count31 = 0;
-                        let count28 = 0;
-                        let count29 = 0;
-                        for(var i = mmReturn + 1; i < mmActual; i++)
-                        {
-                            if(Object.keys(arr30).includes(i.toString())) count30++;
-                            //if the month is jan/mar/may/jul/aug/oct/dec in Return_date 
-                            else if(Object.keys(arr31).includes(i.toString())) count31++;
-                            else 
-                            {   //the month is 'feb'
-                                //checking leap year or not
-                                if( yyyyReturn % 4 == 0 ) count29++;
-                                else count28++;
-                            }
-                        }
-                        Charges_Rs = Charges_Rs + (count30*30 + count31*31 + count29*29 + count28*28) + ddActual
-                    }
-                }
+            let Charges_Rs = 0;
+            // console.log(findTheBook.Charges_Rs);
+            if(findTheBook) 
+            {Charges_Rs = fineCalc(findTheBook.Return_date, Actual_return_date);
+                console.log(Charges_Rs);}
+            else{
+                res.status(400).json({
+                    success : false,
+                    message : "The book does not exist"
+                })
             }
-            const ChargesCalculated = await bookModel.findByIdAndUpdate({_id:bookId},{Charges_Rs})
+            const ChargesCalculated = await bookModel.findByIdAndUpdate({_id:bookId},{
+                Actual_return_date, Charges_Rs})
             if(ChargesCalculated)
             {
                 res.status(200).json({
@@ -354,7 +264,7 @@ catch(error){
             {
                 res.status(400).json({
                     status: false,
-                    messgae : "The selected book does not exist"
+                    messgae : "Something went wrong"
                 })
             }
         }
---
+++ utils/calc.js
@@ -0,0 +1,167 @@
+/**
+ * @description object for months having 31 days
+ * @scope Global
+ */
+ const arr31 = {
+    1 : "january",
+    3 : "march",
+    5:  "may",
+    7:  "july",
+    8: "august",
+    10: "october",
+    12 : "december",
+    arr31Method: function(val){
+            return Object.keys(this).includes(val.toString())
+    }
+}
+
+/**
+ * @description object for months having 30 days
+ * @scope Global
+ */
+const arr30 = {
+    4 : "april",
+    6 : "june",
+    9:  "september",
+    11:  "november",
+    arr30Method: function(val){
+        return Object.keys(this).includes(val.toString())
+}
+}
+
+
+
+/**
+ * @description Return_date calculation
+ * @param Issue_date, Return_date(this is empty)
+ * @Issue_date_format dd-mm-yyyy
+ */
+export const returnDateCalc = (Issue_date,Return_date)=>{
+    
+    let dd = 7 + Number(Issue_date.substr(0,2));//'substr(0,2)' extract substring from position 0 with length=2
+    let mm = Number(Issue_date.substr(3,2));//'substr(3,2)' extract substring from position 3 with length=2
+    let yyyy = Number(Issue_date.substr(6));//'substr(6)' extract rest of the substring from position 6
+
+    //if the month is apr/jun/sep/nov
+    if(arr30.arr30Method(mm)){
+        if(Math.floor(dd/30) > 0) mm = mm + 1;
+        dd = dd % 30;
+    }   
+    //if the month is jan/mar/may/jul/aug/oct/dec  
+    else if(arr31.arr31Method(mm)) {
+        if(Math.floor(dd/31) > 0) mm = mm + 1;
+        dd = dd % 31;
+    }       
+    else {
+        //checking leap year or not
+            if( yyyy % 4 == 0 ) {
+                if( Math.floor(dd/29)>0) mm = mm + 1;
+                dd = dd % 29;
+            }
+            else { 
+                if( Math.floor(dd/28)>0) mm = mm + 1;
+                dd = dd % 28;
+            }
+        }
+    if( Math.floor(mm/12) > 0) {yyyy = yyyy + 1; mm = mm % 12;}
+    let ddS = (dd < 10) ? ('0'+ dd.toString()) : dd.toString()
+    let mmS = (mm < 10) ? ('0'+ mm.toString()) : mm.toString()
+    let yyS = yyyy.toString()
+    Return_date = ddS + '-'+ mmS + '-' + yyS
+    return Return_date
+}
+
+
+
+
+/**
+ * @description Late fine/charges calculation
+ * @param Return_date, Actual_return_date, Charges_rs(empty)
+ * @Return_date_format dd-mm-yyyy
+ * @Actual_return_date_format dd-mm-yyyy
+ */
+export const fineCalc = (Return_date, Actual_return_date)=>{
+
+    let Charges_Rs = 0;
+
+    let yyyyReturn = Number(Return_date.substring(6))//'substring(6)' extract rest of the substring from position 6
+    let yyyyActual = Number(Actual_return_date.substring(6))
+
+    let mmReturn = Number(Return_date.substring(3,5))//'substr(3,5)' extract substring from position 3 to 4
+    let mmActual = Number(Actual_return_date.substring(3,5))
+
+    let ddReturn = Number(Return_date.substring(0,2))//'substring(0,2)' extract substring from position 0 to 1
+    let ddActual = Number(Actual_return_date.substring(0,2))
+
+    let yCountLeap = 0, yCountLeapNot = 0, count31 = 0, count30 = 0, count29 = 0, count28 = 0;
+
+    
+    function countRemainingDays(val){
+        //if the current month is apr/jun/sep/nov in Return_date
+        if(arr30.arr30Method(val)) Charges_Rs = 30 - ddReturn;
+        //if the current month is jan/mar/may/jul/aug/oct/dec in Return_date 
+        else if(arr31.arr31Method(val)) Charges_Rs = 31 - ddReturn;
+        else 
+        {
+            if( yyyyReturn % 4 == 0 ) Charges_Rs = 29 - ddReturn;
+            else Charges_Rs = 28 - ddReturn;
+        }
+    }
+
+    function monthCountTotal(val){
+        if(arr30.arr30Method(val)) count30++;
+        else if(arr31.arr31Method(val)) count31++;
+            else 
+            {   //the month is 'feb'
+                //checking leap year or not
+                if( yyyyActual % 4 == 0 ) count29++;
+                else count28++;
+            }
+    }
+
+
+    //book returned in the same year
+    if(yyyyActual-yyyyReturn == 0)
+    {
+        //book returned in the same month
+        if(mmActual-mmReturn == 0) Charges_Rs = ddActual - ddReturn; 
+        
+        //book returned in later months
+        else if(mmActual-mmReturn > 0)
+        {
+            countRemainingDays(mmReturn)
+            for(var i = mmReturn + 1; i < mmActual; i++)
+            {
+                monthCountTotal(i)
+            }
+            Charges_Rs = Charges_Rs + (count30*30 + count31*31 + count29*29 + count28*28) + ddActual
+        }
+    }
+
+    //book returned in later year,not in the same year
+    else if(yyyyActual-yyyyReturn > 0)
+    {
+        countRemainingDays(mmReturn)
+        
+        //upto december charges is calculated
+        for(var i = mmReturn + 1; i <= 12; i++)
+        {
+            monthCountTotal(i)
+        }
+        //in between no. of years calculation
+        for(var i = yyyyReturn+1 ;i < yyyyActual; i++)
+        {
+            if( i % 4 == 0 ) yCountLeap++;
+            else yCountLeapNot++;
+        }
+        for(var i = 1; i < mmActual; i++)
+        {
+            monthCountTotal(i)
+        }
+        Charges_Rs = Charges_Rs + (yCountLeap*366 + yCountLeapNot*365) + (count30*30 + count31*31 + count29*29 + count28*28) + ddActual
+    }
+    else{
+        Charges_Rs = -1
+    }
+    return Charges_Rs
+}
